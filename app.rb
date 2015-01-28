712f85cfa336df669f78667545ba046da619253d
lcostantini <costantinileandro1@gmail.com>
2015-01-28 15:06:28 -0300

    Add logs in the view.

--- app.rb
+++ app.rb
@@ -17,8 +17,12 @@ Cuba.define do
   on post do
     on "publish/:topic/:payload" do |topic, payload|
       begin
+        unless CLIENT.connected?
+          CLIENT.refresh_connection
+          res.write "Established connection to broker '#{CLIENT.broker}' and port '#{CLIENT.port}'\n"
+        end
         CLIENT.publish! payload, topic_out: topic
-        res.write "Public the value '#{payload}' in channel '#{topic}'"
+        res.write "Published the value '#{payload}' in channel '#{topic}'"
       rescue Timeout::Error
         res.write "Connection timed out. Couldn't publish on broker"
       rescue Exception => e
